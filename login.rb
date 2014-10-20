889edba1358e30fc22a16de43c8991575fc28752
Mosheyur Rahman <mosheyur@mosheyur.therapbd.net>
2014-10-20 12:12:42 +0600

    this is a commit message

---
+++ login.rb
@@ -0,0 +1,20 @@
+module Login
+  def Login.welcome
+    user_choice = 3
+  puts "welcome to facebook. it is free and always will be."
+  print "press 1-3\n"
+  print "1.login\t2.signup\t3.exit\n"
+  while !(/[123]/.match(user_choice = gets))
+    print "press 1-3\n"
+  print "1.login\t2.signup\t3.exit\n"
+    
+  end
+  return user_choice.strip.to_i
+  end
+ 
+end
+
+
+  
+  
+  
